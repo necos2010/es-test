67aa51bb177d360a2d5345712a5e13f5d2879528
Mirumar <bosithabibullaev@gmail.com>
2025-08-18 23:44:21 +0500

    Fixed Header Logo

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,15 +1,11 @@
 import "./Header.css";
-import YellowLogo from "../assets/Yellow-logo.svg";
+import YellowLogo from "../assets/Header-logo.png";
 
 function Header() {
   return (
     <header className="Header">
       <div className="Header-wrapper">
         <img src={YellowLogo} alt="Edford School logo" className="Header-logo" />
-        <div className="Header-text">
-          <p className="Header-edford">Edford</p>
-          <p className="Header-school">School</p>
-        </div>
       </div>
     </header>
   );
